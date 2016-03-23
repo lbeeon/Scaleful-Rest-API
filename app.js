00462c80c5336181c9c5e7368bfb41b92c70986c
lbeeon <lbeeon@hotmail.com>
2016-03-23 23:41:10 +0800

    Router path conflict notify

--- app.js
+++ app.js
@@ -18,19 +18,25 @@ logger.fatal('[fatal]....');
 
 console.log(new env().get_env());
 
+var app = express();
+var router_index = Array();
+
 var path = "./routes";
 var module_list = fs.readdir(path, function(err, data){
     data.forEach(function(route_path){
         var appRouter = require(path + "/" + route_path);
         if (appRouter.is_routes){
             app.use('/', appRouter.appRouter);
+            if (router_index.filter(function(n){  return appRouter.path_list.indexOf(n) > -1;}).length > 0){
+            	throw Error('Router path was registered!!');
+            }
+            router_index = router_index.concat(appRouter.path_list);
         }else{
             console.log(route_path);
         }
     });
 });
 
-var app = express();
 // use body parser so we can get info from POST and/or URL parameters
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json());
